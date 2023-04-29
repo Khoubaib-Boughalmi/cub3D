261fb80ab0ceeae7b8678cd9ccc9ad022b210f24
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-04-29 01:56:53 +0100

    reducing resolution still lagging

--- src/cub3d.c
+++ src/cub3d.c
@@ -15,33 +15,32 @@
 t_vars *g_vars;
 
 int g_map[8][8] = {
-	{1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 1, 1, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 1, 0, 0, 1, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 1, 0, 1, 0, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1}
-};
+	 {1, 1, 1, 1, 1, 1, 1, 1},
+	 {1, 0, 0, 0, 0, 0, 0, 1},
+	 {1, 0, 0, 1, 1, 0, 0, 1},
+	 {1, 0, 0, 0, 0, 0, 0, 1},
+	 {1, 0, 1, 0, 0, 1, 0, 1},
+	 {1, 0, 0, 0, 0, 0, 0, 1},
+	 {1, 0, 0, 1, 0, 1, 0, 1},
+	 {1, 1, 1, 1, 1, 1, 1, 1}};
 
 int32_t create_color(int32_t r, int32_t g, int32_t b, int32_t a)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
 float distance_to_wall(float px, float py, float wx, float wy, float angle_rad)
 {
-	return (sqrt((wy - py)*(wy - py) + (wx - px)*(wx - px)));
+	return (sqrt((wy - py) * (wy - py) + (wx - px) * (wx - px)));
 }
 
 void draw_wall(t_vars *vars, int r, int lineH, int32_t color)
 {
-   int x1 = r * 4 + 4;
-	double ligne_offset = 220 - lineH/2;
-   int y1 = 220 - lineH/2;
+	int x1 = r * 16 + 16;
+	double ligne_offset = 220 - lineH / 2;
+	int y1 = 220 - lineH / 2;
 	// Draw vertical line with specified line width
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 16; i++)
 	{
 		for (int y = y1; y <= lineH + (int)ligne_offset; y++)
 		{
@@ -56,7 +55,7 @@ void clean_window(t_vars *vars)
 	{
 		for (int x = 0; x < 256; x++)
 		{
-			mlx_put_pixel(vars->img, y, x, create_color(50,150,255,255 - x));
+			mlx_put_pixel(vars->img, y, x, create_color(50, 150, 255, 255 - x));
 		}
 	}
 
@@ -64,16 +63,15 @@ void clean_window(t_vars *vars)
 	{
 		for (int x = 256; x < 512; x++)
 		{
-			mlx_put_pixel(vars->img, y, x, create_color(10,200,50, x - 255));
+			mlx_put_pixel(vars->img, y, x, create_color(10, 200, 50, x - 255));
 		}
 	}
 }
 
-
-void	draw_tile(t_vars *vars, int y, int x)
+void draw_tile(t_vars *vars, int y, int x)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 	uint32_t color_black = create_color(0, 0, 0, 255);
 	uint32_t color_white = create_color(255, 255, 255, 255);
 	uint32_t color_grey = create_color(160, 60, 60, 255);
@@ -83,19 +81,18 @@ void	draw_tile(t_vars *vars, int y, int x)
 		j = -1;
 		while (++j < 16)
 		{
-			mlx_put_pixel(vars->img, (16 * x) + i, (16 * y) + j, color_black);
-			if(g_map[y][x])
+			// mlx_put_pixel(vars->img, (16 * x) + i, (16 * y) + j, color_black);
+			if (g_map[y][x])
 				mlx_put_pixel(vars->img, (16 * x) + i, (16 * y) + j, color_grey);
-			if(j == 15 || i == 15)
+			if (j == 15 || i == 15)
 				mlx_put_pixel(vars->img, (16 * x) + i, (16 * y) + j, color_white);
 		}
 	}
-
 }
-void	draw_map(t_vars *vars)
+void draw_map(t_vars *vars)
 {
-	int	x;
-	int	y;
+	int x;
+	int y;
 
 	y = -1;
 	while (++y < 8)
@@ -104,60 +101,58 @@ void	draw_map(t_vars *vars)
 		while (++x < 8)
 			draw_tile(vars, y, x);
 	}
-
 }
-
 
 void draw_ray(t_vars *vars)
 {
 	// t_ray	ray;
-	int	      r;
-	int	      mx;
-	int			my;
-	int			mp;
-	int			dof;
-	double      rx;
-	double		ry;
-	double		ra;
-	double		xo; //x offset
-	double		yo; //y offset
-	double		h_dist;
-	double		h_x;
-	double		h_y;
-	double		v_dist;
-	double		v_x;
-	double		v_y;
-	double		f_dist;
+	int r;
+	int mx;
+	int my;
+	int mp;
+	int dof;
+	double rx;
+	double ry;
+	double ra;
+	double xo; // x offset
+	double yo; // y offset
+	double h_dist;
+	double h_x;
+	double h_y;
+	double v_dist;
+	double v_x;
+	double v_y;
+	double f_dist;
 
 	ra = vars->player.angle - DEG * 30;
 	if (ra < 0)
 		ra += 2 * PI;
 	if (ra > 2 * PI)
 		ra -= 2 * PI;
-	
-	for (int i = 0; i < 256 ; i++)
+
+	for (int i = 0; i < 60; i++)
 	{
-		h_dist = 100000;		
-		h_x = vars->player.x;		
-		h_y = vars->player.y;		
+		h_dist = 100000;
+		h_x = vars->player.x;
+		h_y = vars->player.y;
 		/* horzontal check*/
 		dof = 0;
-		float aTan = -1/tan(ra);
-		if (ra > PI) //facing down
+		float aTan = -1 / tan(ra);
+		if (ra > PI) // facing down
 		{
-			ry = (((int)vars->player.y>>6)<<6)-0.0001;
+			ry = (((int)vars->player.y >> 6) << 6) - 0.0001;
 			rx = (vars->player.y - ry) * aTan + vars->player.x;
 			yo = -64;
 			xo = -yo * aTan;
 		}
-		if (ra < PI) //facing up
+		if (ra < PI) // facing up
 		{
-			ry = (((int)vars->player.y>>6)<<6) + 64;
+			ry = (((int)vars->player.y >> 6) << 6) + 64;
 			rx = (vars->player.y - ry) * aTan + vars->player.x;
 			yo = 64;
 			xo = -yo * aTan;
 		}
-		 if(ra == 0 || ra == PI)
+		if (ra == 0 || ra == PI)
 		{
 			rx = vars->player.x;
 			ry = vars->player.y;
@@ -167,7 +162,7 @@ void draw_ray(t_vars *vars)
 		{
 			mx = (int)(rx) / 64;
 			my = (int)(ry) / 64;
-			if(mx >= 0 && my >= 0 && mx < 8 && my < 8 && g_map[my][mx] == 1)
+			if (mx >= 0 && my >= 0 && mx < 8 && my < 8 && g_map[my][mx] == 1)
 			{
 				h_x = rx;
 				h_y = ry;
@@ -181,28 +176,28 @@ void draw_ray(t_vars *vars)
 				dof++;
 			}
 		}
-		
+
 		/* vertical check*/
 		dof = 0;
-		v_dist = 100000;		
-		v_x = vars->player.x;		
-		v_y = vars->player.y;		
+		v_dist = 100000;
+		v_x = vars->player.x;
+		v_y = vars->player.y;
 		float nTan = -tan(ra);
-		if (ra > PI2 && ra < PI3) //facing left
+		if (ra > PI2 && ra < PI3) // facing left
 		{
-			rx = (((int)vars->player.x>>6)<<6)-0.0001;
+			rx = (((int)vars->player.x >> 6) << 6) - 0.0001;
 			ry = (vars->player.x - rx) * nTan + vars->player.y;
 			xo = -64;
 			yo = -xo * nTan;
 		}
-		if (ra < PI2 || ra > PI3) //facing right
+		if (ra < PI2 || ra > PI3) // facing right
 		{
-			rx = (((int)vars->player.x>>6)<<6) + 64;
+			rx = (((int)vars->player.x >> 6) << 6) + 64;
 			ry = (vars->player.x - rx) * nTan + vars->player.y;
 			xo = 64;
 			yo = -xo * nTan;
 		}
-		if(ra == PI2 || ra == PI3) //up or down
+		if (ra == PI2 || ra == PI3) // up or down
 		{
 			rx = vars->player.x;
 			ry = vars->player.y;
@@ -212,7 +207,7 @@ void draw_ray(t_vars *vars)
 		{
 			mx = (int)(rx) / 64;
 			my = (int)(ry) / 64;
-			if(mx >= 0 && my >= 0 && mx < 8 && my < 8 && g_map[my][mx] == 1)
+			if (mx >= 0 && my >= 0 && mx < 8 && my < 8 && g_map[my][mx] == 1)
 			{
 				v_x = rx;
 				v_y = ry;
@@ -226,7 +221,7 @@ void draw_ray(t_vars *vars)
 				dof++;
 			}
 		}
-		if(v_dist < h_dist)
+		if (v_dist < h_dist)
 		{
 			rx = v_x;
 			ry = v_y;
@@ -238,65 +233,65 @@ void draw_ray(t_vars *vars)
 			ry = h_y;
 			f_dist = h_dist;
 		}
-		draw_map(vars);
-		put_line(vars->mlx, vars->win, (vars->player.x * 16) / 64, (vars->player.y * 16) / 64, (rx* 16) / 64, (ry* 16) / 64, create_color(255,255,0,255), 128,128);
+		put_line(vars->mlx, vars->win, (vars->player.x * 16) / 64, (vars->player.y * 16) / 64, (rx * 16) / 64, (ry * 16) / 64, create_color(255, 255, 0, 255), 128, 128);
 		double fish_eye_new_angle = vars->player.angle - ra;
-		if(fish_eye_new_angle < 0)
-			fish_eye_new_angle += 2*PI;
-		if(fish_eye_new_angle > 2*PI)
-			fish_eye_new_angle -= 2*PI;
+		if (fish_eye_new_angle < 0)
+			fish_eye_new_angle += 2 * PI;
+		if (fish_eye_new_angle > 2 * PI)
+			fish_eye_new_angle -= 2 * PI;
 		f_dist = f_dist * cos(fish_eye_new_angle);
-		float line_height = (64 * 420)/f_dist;
-		if(line_height > 440)
+		float line_height = (64 * 420) / f_dist;
+		if (line_height > 440)
 			line_height = 440;
-		if(v_dist > h_dist)
-			draw_wall(vars, i, line_height, create_color(150,150,150, 256 - i ));
+		if (v_dist > h_dist)
+			draw_wall(vars, i, line_height, create_color(150, 150, 150, 256 - i));
 		else
-			draw_wall(vars, i, line_height, create_color(150,150,150, i));
-		ra = ra + DEG / 4;
+			draw_wall(vars, i, line_height, create_color(150, 150, 150, i));
+		ra = ra + DEG;
 		if (ra < 0)
 			ra += 2 * PI;
 		if (ra > 2 * PI)
 			ra -= 2 * PI;
 	}
+	// draw_map(vars);
 }
 
-void	draw_partcle(t_vars *vars)
+void draw_partcle(t_vars *vars)
 {
-	int			radius;
-	int 		i;
-	int 		j;
-	int 		py;
-	int 		px;
-	uint32_t	color;
+	int radius;
+	int i;
+	int j;
+	int py;
+	int px;
+	uint32_t color;
 
 	i = -1;
 	radius = 3;
 	px = (((vars->player.x) * 16) / 64);
 	py = (((vars->player.y) * 16) / 64);
 	printf("posx %f %f\n", vars->player.x, vars->player.y);
-	for (int x = 0; x < 128 ; x++)
+	for (int x = 0; x < 128; x++)
 	{
-		for (int y = 0; y < 128 ; y++)
+		for (int y = 0; y < 128; y++)
 		{
 			if ((x - px) * (x - px) + (y - py) * (y - py) < radius * radius)
-				mlx_put_pixel(vars->img, x ,y , create_color(255, 255, 255, 255));
+				mlx_put_pixel(vars->img, x, y, create_color(255, 255, 255, 255));
 		}
 	}
-	put_line(vars->mlx, vars->win, (((vars->player.x) * 16) / 64), (((vars->player.y) * 16) / 64), ((vars->player.x) * 16) / 64 + vars->player.dx * 10, (vars->player.y * 16) / 64 + vars->player.dy * 10 , create_color(0,255,0,255), 128, 128);
+	put_line(vars->mlx, vars->win, (((vars->player.x) * 16) / 64), (((vars->player.y) * 16) / 64), ((vars->player.x) * 16) / 64 + vars->player.dx * 10, (vars->player.y * 16) / 64 + vars->player.dy * 10, create_color(0, 255, 0, 255), 128, 128);
 }
 
-
-void	redraw(t_vars *vars)
+void redraw(t_vars *vars)
 {
 	clean_window(vars);
-	draw_ray(vars);
+	draw_map(vars);
 	draw_partcle(vars);
+	draw_ray(vars);
 }
 
 int key_press_handler(mlx_key_data_t keydata, void *param)
 {
-	t_vars	*vars;
+	t_vars *vars;
 	float angle;
 	float dxright;
 	float dyright;
@@ -304,7 +299,7 @@ int key_press_handler(mlx_key_data_t keydata, void *param)
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_RIGHT))
 	{
 		vars->player.angle += 0.1;
-		if(vars->player.angle > 2*PI)
+		if (vars->player.angle > 2 * PI)
 			vars->player.angle -= 2 * PI;
 		vars->player.dx = 5 * cos(vars->player.angle);
 		vars->player.dy = 5 * sin(vars->player.angle);
@@ -312,70 +307,68 @@ int key_press_handler(mlx_key_data_t keydata, void *param)
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
 	{
 		vars->player.angle -= 0.1;
-		if(vars->player.angle < 0)
+		if (vars->player.angle < 0)
 			vars->player.angle += 2 * PI;
 		vars->player.dx = 5 * cos(vars->player.angle);
 		vars->player.dy = 5 * sin(vars->player.angle);
 	}
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_W))
 	{
-		int x=(int)floor((vars->player.x+(vars->player.dx *2))/64);
-		int y=(int)floor((vars->player.y+(vars->player.dy *2))/64);
-		int factx=(int)floor((vars->player.x)/64);
-		int facty=(int)floor((vars->player.y)/64);
-		if(g_map[y][x]!=1)
+		int x = (int)floor((vars->player.x + (vars->player.dx * 2)) / 64);
+		int y = (int)floor((vars->player.y + (vars->player.dy * 2)) / 64);
+		int factx = (int)floor((vars->player.x) / 64);
+		int facty = (int)floor((vars->player.y) / 64);
+		if (g_map[y][x] != 1)
 		{
 			vars->player.x += vars->player.dx;
 			vars->player.y += vars->player.dy;
 		}
 		else
 		{
-			if(g_map[facty][x]!=1)
+			if (g_map[facty][x] != 1)
 			{
 				vars->player.x += vars->player.dx;
 				// vars->player.y += vars->player.dy;
-				
-			}		
-			if(g_map[y][factx]!=1)
+			}
+			if (g_map[y][factx] != 1)
 			{
 				// vars->player.x += vars->player.dx;
 				vars->player.y += vars->player.dy;
-				
-			}	
+			}
 		}
 	}
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_S))
 	{
-			vars->player.x -= vars->player.dx;
-			vars->player.y -= vars->player.dy;
+		vars->player.x -= vars->player.dx;
+		vars->player.y -= vars->player.dy;
 	}
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_A))
 	{
-		angle = vars->player.angle-PI/2;
-		if(angle > 2*PI)
+		angle = vars->player.angle - PI / 2;
+		if (angle > 2 * PI)
 			angle -= 2 * PI;
-		if(angle < 0)
+		if (angle < 0)
 			angle += 2 * PI;
-		
+
 		dxright = 5 * cos(angle);
 		dyright = 5 * sin(angle);
-		vars->player.x +=dxright;
+		vars->player.x += dxright;
 		vars->player.y += dyright;
 	}
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_D))
 	{
-		angle = vars->player.angle+PI/2;
-		if(angle > 2*PI)
+		angle = vars->player.angle + PI / 2;
+		if (angle > 2 * PI)
 			angle -= 2 * PI;
-		if(angle < 0)
+		if (angle < 0)
 			angle += 2 * PI;
-		
+
 		dxright = 5 * cos(angle);
 		dyright = 5 * sin(angle);
-		vars->player.x +=dxright;
+		vars->player.x += dxright;
 		vars->player.y += dyright;
 	}
-	if (mlx_is_key_down(vars->mlx, MLX_KEY_ESCAPE)) //free shit
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_ESCAPE)) // free shit
 		exit(0);
 	redraw(vars);
 	return (0);
@@ -383,16 +376,16 @@ int key_press_handler(mlx_key_data_t keydata, void *param)
 
 void mouse_handler(double xpos, double ypos, void *param)
 {
-	t_vars	*vars;
+	t_vars *vars;
 	vars = (t_vars *)param;
-	if(xpos > vars->player.prev_xpos)
+	if (xpos > vars->player.prev_xpos)
 	{
 		if (xpos < 512)
 			xpos = 512;
-		else if(xpos > 1024)
+		else if (xpos > 1024)
 			xpos = 1024;
-		vars->player.angle += (3/xpos * 5);
-		if(vars->player.angle > 2*PI)
+		vars->player.angle += (3 / xpos * 5);
+		if (vars->player.angle > 2 * PI)
 			vars->player.angle -= 2 * PI;
 		vars->player.dx = 5 * cos(vars->player.angle);
 		vars->player.dy = 5 * sin(vars->player.angle);
@@ -401,10 +394,10 @@ void mouse_handler(double xpos, double ypos, void *param)
 	{
 		if (xpos < 512)
 			xpos = 512;
-		else if(xpos > 1024)
+		else if (xpos > 1024)
 			xpos = 1024;
-		vars->player.angle -= (3/xpos * 5);
-		if(vars->player.angle < 0)
+		vars->player.angle -= (3 / xpos * 5);
+		if (vars->player.angle < 0)
 			vars->player.angle += 2 * PI;
 		vars->player.dx = 5 * cos(vars->player.angle);
 		vars->player.dy = 5 * sin(vars->player.angle);
@@ -413,15 +406,15 @@ void mouse_handler(double xpos, double ypos, void *param)
 	redraw(vars);
 }
 
-void	render_window(t_vars *vars)
+void render_window(t_vars *vars)
 {
-	int	width;
-	int	height;
+	int width;
+	int height;
 
 	width = vars->window_info.width;
 	height = vars->window_info.height;
 	vars->mlx = mlx_init(width, height, "cub3D", 1);
-	mlx_set_window_limit(vars->mlx,  width - 200, height - 200, width, height);
+	mlx_set_window_limit(vars->mlx, width - 200, height - 200, width, height);
 	vars->img = mlx_new_image(vars->mlx, width, height);
 	clean_window(vars);
 	draw_partcle(vars);
@@ -437,15 +430,15 @@ void	render_window(t_vars *vars)
 	mlx_terminate(vars->mlx);
 }
 
-int	init_vars(void)
+int init_vars(void)
 {
 	g_vars = (t_vars *)malloc(sizeof(t_vars));
-	if(!g_vars)
+	if (!g_vars)
 		return (0);
 	g_vars->window_info.height = 512;
 	g_vars->window_info.width = 1024;
-	g_vars->player.angle = -90 * (PI/180);
-	g_vars->player.angle = 90 * (PI/180);
+	g_vars->player.angle = -90 * (PI / 180);
+	g_vars->player.angle = 90 * (PI / 180);
 	g_vars->player.prev_xpos = 0;
 	g_vars->player.x = 64;
 	g_vars->player.y = 64;
@@ -456,9 +449,9 @@ int	init_vars(void)
 
 int main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
-	if(!init_vars())
+	(void)argc;
+	(void)argv;
+	if (!init_vars())
 		return (0);
 	render_window(g_vars);
 	return (0);
